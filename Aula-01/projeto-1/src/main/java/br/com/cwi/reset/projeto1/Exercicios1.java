de5189730b62b8099570f47121081ba9e0f08d07
vitoramoretti <amoretti.vitor@gmail.com>
2021-10-02 16:48:23 -0300

    added Aula-01 for exercises

---
+++ Aula-01/projeto-1/src/main/java/br/com/cwi/reset/projeto1/Exercicios1.java
@@ -0,0 +1,28 @@
+package br.com.cwi.reset.projeto1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercicios1 {
+
+    public Integer somarLista(List<Integer> numeros) {
+        return 10;
+    }
+
+    public Double calcularMedia(List<Integer> numeros) {
+        return 10.0;
+    }
+
+    public Integer obterMaiorNumero(List<Integer> numeros) {
+        return 10;
+    }
+
+    public String obterPalavraInvertida(String palavra) {
+        return "edcba";
+    }
+
+    public List<Integer> ordenarLista(List<Integer> numeros) {
+        return Arrays.asList(1, 2, 3, 4, 5);
+    }
+}
+
